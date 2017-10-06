7228a4d702cf4589f45c819b20c028e1b94f4130
Karthik Rana <karthik.rana@kdi.kongsberg.com>
2017-10-06 13:51:16 +0530

    Material components module set up

--- src/app/expense/expense.module.ts
+++ src/app/expense/expense.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from "../shared/shared.module";
 import { ExpenseRoutingModule } from "./expense-routing.module";
 import { SubmitExpenseComponent } from './submit-expense/submit-expense.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
-    ExpenseRoutingModule
+    ExpenseRoutingModule,
+    MaterialModule
   ],
   declarations: [SubmitExpenseComponent]
 })
